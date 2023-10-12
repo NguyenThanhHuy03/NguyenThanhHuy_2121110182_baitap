3db6165ca75c496590743c31934c868957da25b1
thanhhuy <huynt605@gmail.com>
2023-10-12 19:19:15 +0700

    huy

--- DataGridView/Form1.cs
+++ DataGridView/Form1.cs
@@ -16,5 +16,30 @@ namespace DataGridView
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text, checkBox1.Checked);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idx = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(idx);
+        }
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            int idx = e.RowIndex;
+            textBox1.Text = dataGridView1.Rows[idx].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[idx].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[idx].Cells[2].Value.ToString();
+            checkBox1.Checked = bool.Parse( dataGridView1.Rows[idx].Cells[0].Value.ToString());
+        }
     }
 }
--- NguyenThanhHuy_2121110182_BT/Form1.cs
+++ NguyenThanhHuy_2121110182_BT/Form1.cs
@@ -16,5 +16,10 @@ namespace NguyenThanhHuy_2121110182_BT
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
