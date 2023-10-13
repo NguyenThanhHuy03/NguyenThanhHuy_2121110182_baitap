600540af7db3a5aae6ec5d64435f295d727e6013
thanhhuy <huynt605@gmail.com>
2023-10-13 13:34:05 +0700

    load hình

---
+++ Picture/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Picture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btFile = new System.Windows.Forms.Button();
+            this.btLeft = new System.Windows.Forms.Button();
+            this.btRight = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btFile
+            // 
+            this.btFile.Location = new System.Drawing.Point(218, 324);
+            this.btFile.Name = "btFile";
+            this.btFile.Size = new System.Drawing.Size(132, 37);
+            this.btFile.TabIndex = 6;
+            this.btFile.Text = "File...";
+            this.btFile.UseVisualStyleBackColor = true;
+            this.btFile.Click += new System.EventHandler(this.btFile_Click);
+            // 
+            // btLeft
+            // 
+            this.btLeft.Location = new System.Drawing.Point(57, 320);
+            this.btLeft.Name = "btLeft";
+            this.btLeft.Size = new System.Drawing.Size(52, 44);
+            this.btLeft.TabIndex = 4;
+            this.btLeft.Text = "<-";
+            this.btLeft.UseVisualStyleBackColor = true;
+            this.btLeft.Click += new System.EventHandler(this.btLeft_Click);
+            // 
+            // btRight
+            // 
+            this.btRight.Location = new System.Drawing.Point(115, 320);
+            this.btRight.Name = "btRight";
+            this.btRight.Size = new System.Drawing.Size(52, 44);
+            this.btRight.TabIndex = 5;
+            this.btRight.Text = "->";
+            this.btRight.UseVisualStyleBackColor = true;
+            this.btRight.Click += new System.EventHandler(this.btRight_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(45, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(151, 167);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btFile);
+            this.Controls.Add(this.btLeft);
+            this.Controls.Add(this.btRight);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btFile;
+        private System.Windows.Forms.Button btLeft;
+        private System.Windows.Forms.Button btRight;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Picture/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture
+{
+    public partial class Form1 : Form
+    {
+        PictureBox pb = new PictureBox();
+        int x = 0;
+        int y = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btFile_Click(object sender, EventArgs e)
+        {
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb.Size = new Size(100, 100);
+            pb.Location = new Point(x, y);
+            this.Controls.Add(pb);
+            pb.ImageLocation = @"C:\Users\ADMIN\OneDrive\Hình ảnh\Saved Pictures\item1.jpg";
+        }
+
+        private void btLeft_Click(object sender, EventArgs e)
+        {
+            x -= 10;
+            pb.Location = new Point(x, y);
+        }
+
+        private void btRight_Click(object sender, EventArgs e)
+        {
+            x += 10;
+            pb.Location = new Point(x, y);
+        }
+    }
+}
